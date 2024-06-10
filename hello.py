913ed7188ac49cd412ef0803f1bb82fb5476e4a8
Monika <monika20@iiserb.ac.in>
2024-06-10 20:50:26 +0530

    update hw1

--- hello.py
+++ hello.py
@@ -1,13 +1,2 @@
 def hello_world():
-	return "Hello World!"
-
-print(hello_world())
-
-def hello_world_n(N):
-	TMP = "Hello world! "
-	return (TMP*N)
-print(hello_world_n(5))
-<<<<<<< HEAD
-
-=======
->>>>>>> 9845c30590ea9f1f869ee231d965f55f021f7a79
+	return "Hello"
